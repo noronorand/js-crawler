ae26375ca0e5a4a55953f7d0c09d981a303dfc59
Tom.Jukic <jomotomo@gmail.com>
2018-02-25 03:15:13 -0500

    format

--- src/main/java/tj/scalable/WebLoader.java
+++ src/main/java/tj/scalable/WebLoader.java
@@ -16,7 +16,7 @@ public class WebLoader {
 	private String defaultAgent = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2";
 	private int connTO = 1000;
 	private int readTO = 5000;
-	
+
 	public WebLoader() {
 
 	}
@@ -37,7 +37,8 @@ public class WebLoader {
 			}
 			rd.close();
 		} catch (IOException e) {
-			log.error("Failed to load {}",url,e);;
+			log.error("Failed to load {}", url, e);
+			;
 			throw e;
 		}
 		return result.toString();
